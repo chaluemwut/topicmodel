b19c06730ccc432db755682bba9687951fce470e
chaluemwut <chaluemwut@hotmail.com>
2015-08-30 23:18:10 +0700

    add gensim

--- src/lab_lda.py
+++ src/lab_lda.py
@@ -16,5 +16,37 @@ def lda_test():
         topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
         print('Topic {}: {}'.format(i, ' '.join(topic_words)))
 
+# http://radimrehurek.com/gensim/tut1.html
+def gensim():
+    from gensim import corpora, models, similarities
+    documents = ["Human machine interface for lab abc computer applications",
+                 "A survey of user opinion of computer system response time",
+                 "The EPS user interface management system",
+                 "System and human system engineering testing of EPS",
+                 "Relation of user perceived response time to error measurement",
+                 "The generation of random binary unordered trees",
+                 "The intersection graph of paths in trees",
+                 "Graph minors IV Widths of trees and well quasi ordering",
+                 "Graph minors A survey"]
+    stoplist = set('for a of the and to in'.split())
+    texts = [[word for word in document.lower().split() if word not in stoplist]
+             for document in documents]
+
+    from collections import defaultdict
+    frequency = defaultdict(int)
+    for text in texts:
+        for token in text:
+            frequency[token] += 1
+    
+    texts = [[token for token in text if frequency[token] > 1]
+             for text in texts]
+    
+    from pprint import pprint  # pretty-printer
+    pprint(texts)
+    dictionary = corpora.Dictionary(texts)
+    dictionary.save('/tmp/deerwester.dict') # store the dictionary, for future reference
+    print(dictionary)
+    print(dictionary.token2id)
+            
 if __name__ == '__main__':
-    lda_test()
+    gensim()
